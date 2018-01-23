73465d95cde31f3e0c50a72353a435a129dc8ebc
Patrick Bailey <patrickbailey13@gmail.com>
2018-01-22 23:01:57 -0800

    intro to creating a react template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,40 @@ console.log('App.js is running');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App!!!"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App!!!"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some text"
+    )
 );
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Patrick Bailey"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 35"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Junction City"
+    )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App!!!</h1>;
+var template = (
+    <div>
+        <h1>Indecision App!!!</h1>
+        <p>This is some text</p>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Patrick Bailey</h1>
+        <p>Age: 35</p>
+        <p>Location: Junction City</p>
+    </div>
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
