770175892e4a0b8863e569b0126f35934f22794d
Patrick Bailey <patrick@ieqtechnology.com>
2018-01-23 17:03:32 -0800

    13. const and let

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,9 @@
+var nameVar = 'Andrew';
+nameVar = 'Mike';
+console.log('nameVar', nameVar);
+
+let nameLet = 'Jen';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
