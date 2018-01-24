05576d129e915f0df800122095dfc936ce2ab6aa
Patrick Bailey <patrick@ieqtechnology.com>
2018-01-23 16:45:53 -0800

    12. conditional rendering

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ console.log('App.js is running');
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -18,15 +21,21 @@ var template = (
 
 var user = {
     name: 'Patrick',
-    age: 36,
-location : 'Junction City'
+    age: 27,
+    location : 'Junction City'
 };
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    } 
+}
 
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
