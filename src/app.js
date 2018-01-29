5184c6fc5f4bf56830402ed142020fc72e8a9e6b
Patrick Bailey <patrickbailey13@gmail.com>
2018-01-28 20:45:24 -0800

    component props

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+class IndecisionApp extends React.Component {
+    render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options = ['Thing One', 'Thing Two', 'Thing Four'];
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options options={options} />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -24,7 +40,9 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                Options component here
+                {
+                    this.props.options.map((opt) => <Option key={opt} optionText={opt} />)
+                }
             </div>
         );
     }
@@ -40,13 +58,14 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-);
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.optionText}
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
