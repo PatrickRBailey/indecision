eda4f1bb2478d5e25d03426e317bc3a45f518a2a
Patrick Bailey <patrickbailey13@gmail.com>
2018-01-28 21:26:21 -0800

    method binding

--- src/app.js
+++ src/app.js
@@ -26,10 +26,13 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('Handle Pick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
         
@@ -37,9 +40,19 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+
+    }
+    handleRemoveAll() {
+        console.log(this.props.options);
+        alert('Handle Remove All');
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((opt) => <Option key={opt} optionText={opt} />)
                 }
@@ -49,10 +62,21 @@ class Options extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+
+        if (option) {
+            alert(option);
+        } 
+    }
     render() {
         return (
             <div>
-                Add option component here
+                <form onSubmit={this.handleAddOption}>
+                    <input type='text' name='option' />
+                    <button>Add Option</button>
+                </form>
             </div>
         );
     }
