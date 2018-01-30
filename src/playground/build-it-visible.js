114c6136bb7a8a31e147bc42cf273e18e2be1cf9
Patrick Bailey <patrickbailey13@gmail.com>
2018-01-29 21:59:32 -0800

    converting build it app to component and state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,24 +1,28 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = { 
+            visibility: false 
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        });
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+                {this.state.visibility && (<p>These are some details</p>)}
+            </div>
+         );
+    }
+}
+ 
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-const toggleDetails = () => { 
-    visibility = !visibility;
-    render()
-};
-
-const appRoot = document.getElementById("app");
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleDetails}>{visibility ? 'Hide details' : 'Show details'}</button>
-            {visibility && (
-                <p>These are some details</p>
-            )}
-            
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
-
-render();
