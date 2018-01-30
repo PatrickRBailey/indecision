751cfe9f246033f9af12bc16eaa583b16175775a
Patrick Bailey <patrick@ieqtechnology.com>
2018-01-30 10:19:29 -0800

    adding form validation and concat on an array

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ class IndecisionApp extends React.Component {
         super(props);
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options:['Thing One', 'Thing Two', 'Thing Three']
+            options:[]
         }
     }
 
@@ -21,6 +22,18 @@ class IndecisionApp extends React.Component {
         const option = this.state.options[randomNum];
         alert(option);
     }
+    handleAddOption(option) {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists'
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat(option)
+            }
+        });
+    }   
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
@@ -29,14 +42,16 @@ class IndecisionApp extends React.Component {
             <div>
                 <Header title={title} subtitle={subtitle} />
                 <Action 
-                hasOptions={this.state.options.length > 0} 
-                handlePick={this.handlePick}
+                  hasOptions={this.state.options.length > 0} 
+                  handlePick={this.handlePick}
                 />
                 <Options 
-                options={this.state.options} 
-                handleDeleteOptions={this.handleDeleteOptions}
+                  options={this.state.options} 
+                  handleDeleteOptions={this.handleDeleteOptions}
                 />
-                <AddOption />
+                <AddOption 
+                  handleAddOption={this.handleAddOption}
+                />
             </div>
         );
     }
@@ -83,17 +98,27 @@ class Options extends React.Component {
 }
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
     handleAddOption(e) {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
 
-        if (option) {
-            alert(option);
-        } 
+        this.setState(() => {
+            return { error };
+        })
+
     }
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type='text' name='option' />
                     <button>Add Option</button>
