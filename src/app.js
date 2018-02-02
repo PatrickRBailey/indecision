9a7d99e05b72a69318d24755d0c3f54fe7483b92
Patrick Bailey <patrick@ieqtechnology.com>
2018-02-01 16:53:44 -0800

    moving to seperate files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>THIS IS JSX</p>;
-ReactDOM.render(template, document.getElementById('app'));
+
+
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- src/person.js
+++
@@ -1,5 +0,0 @@
-const isAdult = (age) => age >= 18 ? true : false;
-const canDrink = (age) => age >= 21? true: false;
-export default (age) => age >= 65? true: false;
-
-export { isAdult, canDrink };
