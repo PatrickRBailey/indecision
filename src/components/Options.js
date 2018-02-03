ba43234f243ebf553d4e3889c423f36fd20005e1
Patrick Bailey <patrickbailey13@gmail.com>
2018-02-02 21:25:08 -0800

    styling the form

--- src/components/Options.js
+++ src/components/Options.js
@@ -14,10 +14,11 @@ const Options = (props) => (
         </div>
         {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
         {
-            props.options.map((opt) => (
+            props.options.map((opt, index) => (
                 <Option
                     key={opt}
                     optionText={opt}
+                    count={index + 1}
                     handleDeleteOption={props.handleDeleteOption}
                 />
             ))
